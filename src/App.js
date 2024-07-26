6838758a0126a63891140f698794f379f81a26e7
Hasan-Irfan <riyankam82@gmail.com>
2024-07-26 10:33:29 -0700

    m

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { TodoList } from './pages/TodoList';
 import { SignIn } from './pages/SignIn';
 import { Login } from './pages/Login';
+import { useState } from "react";
 
 
 function App() {
+  const [user,setUser]=useState();
+
+  useEffect(()=>{
+    auth.onAuthStateChanged(user=>{
+      setUser(user);
+    })
+  })
+
   return (
     <div className='App'>
       <Router>
         <Routes>
-        <Route path="/" element={<SignIn/>} />
+        <Route path="/" element={user ? <Navigate to ="/ToDoList" /> : <SignIn/>} />
           <Route path="/ToDoList" element={<TodoList/>} />
           <Route path="/login" element={<Login />} />
         </Routes>
